af03e825a200489a7230b7fbfbf7d550e3386e22
Melissa Pizarro <mpizarro03@gmail.com>
2018-05-08 16:28:35 -0600

    backing up

---
+++ js/blog.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+
+
+  fetch("https://theraceblogger.com/feed/").then((res) => {
+    res.text().then((htmlTxt) => {
+      var domParser = new DOMParser()
+      let doc = domParser.parseFromString(htmlTxt, 'text/html')
+      var feedUrl = doc.querySelector('link[type="application/rss+xml"]').href
+    })
+  }).catch(() => console.error('Error in fetching the website'))
+
+
+
+
+
+});
--- js/init.js
+++ js/init.js
@@ -10,25 +10,3 @@
 $(document).ready(function(){
    $('.slider').slider();
  });
-
-
- // document.addEventListener("DOMContentLoaded", function () {
- //
- // let form = document.getElementById('Form')
- // let submit = document.getElementById("submit-button")
- //
- //
- //
- //   	submit.addEventListener("click", function () {
- //     	if(checkbox.checked && !secretInput.value) {
- //       	event.preventDefault();
- //       	alert("enter secret password");
- //    	 } else if (checkbox.checked && secretInput.value !== secretPassword) {
- //      event.preventDefault();
- //      alert("wrong secret password")
- //      }
- //      else if (checkbox.checked && secretInput.value == secretPassword && email.checkValidity() && password.checkValidity())
- //      {alert("good job!")}
- //   })
- //
- // });
