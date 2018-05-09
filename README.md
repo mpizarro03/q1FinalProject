de59b75d404b5a6d8e7f85b5118490050e84161a
Melissa Pizarro <mpizarro03@gmail.com>
2018-05-09 07:29:34 -0600

    MVP done

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# q1FinalProject
-Q1 Final Project
+
+#Q1 Final Project
 
 Color Theme: https://coolors.co/63606d-171923-c6c3c2-3f3f0c-474716
+
 GitHub: https://github.com/mpizarro03/q1FinalProject
+
 Surge:
+
+### Usage
+  - Make requests to: `https://g-theraceblogger.herokuapp.com/?t=the+matrix&y=&plot=short&r=json`
+
+Wireframe: https://snag.gy/jgMNFi.jpg
--- js/blog.js
+++ js/blog.js
@@ -2,11 +2,11 @@
 $(document).ready(function(){
 
 
-  fetch("https://theraceblogger.com/feed/").then((res) => {
+  fetch("https://g-theraceblogger.herokuapp.com/?t=the+matrix&y=&plot=short&r=json").then((res) => {
     res.text().then((htmlTxt) => {
       var domParser = new DOMParser()
       let doc = domParser.parseFromString(htmlTxt, 'text/html')
-      var feedUrl = doc.querySelector('link[type="application/rss+xml"]').href
+      var feedUrl = doc.querySelector('link[type="application/rss+xml"].href')
     })
   }).catch(() => console.error('Error in fetching the website'))
 
