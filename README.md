56368fed30cc7761c8b6934dd3a0430829eda19a
Melissa Pizarro <mpizarro03@gmail.com>
2018-05-10 19:34:53 -0600

    blog page loads articles from wordpress, most recent 3 plus archives

--- README.md
+++ README.md
@@ -5,9 +5,10 @@ Color Theme: https://coolors.co/63606d-171923-c6c3c2-3f3f0c-474716
 
 GitHub: https://github.com/mpizarro03/q1FinalProject
 
+Wireframe: https://snag.gy/jgMNFi.jpg
+
 Surge:
 
-### Usage
-  - Make requests to: `https://g-theraceblogger.herokuapp.com/?t=the+matrix&y=&plot=short&r=json`
+### Usage API
 
-Wireframe: https://snag.gy/jgMNFi.jpg
+Make requests to: `https://g-theraceblogger.herokuapp.com/?t=the+matrix&y=&plot=short&r=json`
--- js/blog.js
+++ js/blog.js
@@ -1,29 +1,40 @@
-
- $(document).ready(function(){
+$(document).ready(function() {
 
   var $xhr = $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftheraceblogger.com%2Ffeed");
   $xhr.done(function(data) {
+    let links = data.items
+    console.log(data)
+    console.log(links);
 
-  // console.log(data.items);
+    //for loop to retrieve articles for archives section
+    for (let i = 0; i < links.length; i++) {
+      let linkValue = links[i].link
+      let titleValue = links[i].title
+      let parent1 = document.getElementsByClassName("archives")[0]
+      let newLink1 = document.createElement("li")
+      let innerHtml = `<a href = ${linkValue}>${titleValue}</a>`
+      newLink1.innerHTML = innerHtml
+      parent1.appendChild(newLink1)
 
-  let links = data.items
-  // console.log(links);
+    }
+  //for loop to retrieve articles for 3 most recent articles (title + link)
+    for (let i = 0; i < 3; i++) {
+      let linkValue = links[i].link
+      let titleValue = links[i].title
+      let pubValue = links[i].pubDate
+      let blogContent = links[i].content
+      let blogPics = links[i].thumbnail
 
-  for(let i = 0; i < links.length; i++){
-    let linkValue = links[i].link
-    console.log(linkValue);
-    let parent1 = document.getElementsByClassName("articles")[0]
-    console.log(parent1)
-    let newLink1 = document.createElement("li")
-    let innerHtml = `<a href = ${linkValue}>test</a>`
-    newLink1.innerHTML = innerHtml
-    parent1.appendChild(newLink1)
-
-  }
-
-})
-    // let parent1 = document.getElementsByClassName("articles1")[0];
-    // let newLink1 = document.createElement("a");
-    // newLink1.innerHTML = links;
-    // parent1.appendChild(newLink1);
+      let parent1 = document.getElementsByClassName("articles")[0]
+      let newLink1 = document.createElement("li")
+      let innerHtml = `<div class="row">
+          <div class="col s12 m8 l12 body1">
+            <h5 class="center-align top-three"><a href = ${linkValue}>${titleValue}</a></h5>
+            <p>${blogContent}</p>
+          </div>
+        </div>`
+      newLink1.innerHTML = innerHtml
+      parent1.appendChild(newLink1)
+    }
+  })
 })
