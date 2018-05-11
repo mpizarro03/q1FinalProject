65c2e81cbf131fad6a0c5dc2fa91f00f90dddc86
Melissa Pizarro <mpizarro03@gmail.com>
2018-05-11 11:26:23 -0600

    final changes for presenatation

--- js/blog.js
+++ js/blog.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
   var $xhr = $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftheraceblogger.com%2Ffeed");
   $xhr.done(function(data) {
     let links = data.items
-    console.log(data)
     console.log(links);
 
     //for loop to retrieve articles for archives section
@@ -12,29 +11,39 @@ $(document).ready(function() {
       let titleValue = links[i].title
       let parent1 = document.getElementsByClassName("archives")[0]
       let newLink1 = document.createElement("li")
-      let innerHtml = `<a href = ${linkValue}>${titleValue}</a>`
+      let innerHtml = `<a target="_blank" href = ${linkValue}>${titleValue}</a>`
       newLink1.innerHTML = innerHtml
       parent1.appendChild(newLink1)
 
     }
   //for loop to retrieve articles for 3 most recent articles (title + link)
-    for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 3; i++) {
       let linkValue = links[i].link
       let titleValue = links[i].title
       let pubValue = links[i].pubDate
-      let blogContent = links[i].content
+      let blogContent = links[i].description
       let blogPics = links[i].thumbnail
 
-      let parent1 = document.getElementsByClassName("articles")[0]
-      let newLink1 = document.createElement("li")
-      let innerHtml = `<div class="row">
-          <div class="col s12 m8 l12 body1">
-            <h5 class="center-align top-three"><a href = ${linkValue}>${titleValue}</a></h5>
-            <p>${blogContent}</p>
+      let topParent = document.getElementsByClassName("articles3")[0]
+      let newChild1 = document.createElement("div")
+      let innerCard =
+      `<div class="col s12 m4">
+          <div class="card card-blog z-depth-5">
+            <div class="card-blog-action card-image waves-effect waves-block waves-light ">
+              <img class="blogpics1" src="${blogPics}">
+            </div>
+            <div class="card-content">
+              <span class="card-title grey-text text-darken-4"><a target="_blank" href = ${linkValue}>${titleValue}</a><i class="material-icons right activator">more_vert</i></span>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4"><a target="_blank" href = ${linkValue}>${titleValue}</a><i class="material-icons right">close</i></span>
+              <p class="black-text">${blogContent}<a target="_blank" href = ${linkValue}>[Read More...]</p>
+            </div>
           </div>
-        </div>`
-      newLink1.innerHTML = innerHtml
-      parent1.appendChild(newLink1)
+      </div>`
+      newChild1.innerHTML = innerCard
+      topParent.appendChild(newChild1)
     }
+
   })
 })
